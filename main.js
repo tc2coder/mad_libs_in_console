3768e6f085e44a4705dde08bd90bf6bc7a67374b
tcullen <traceycw@comcast.net>
2015-06-02 20:30:24 -0700

    message recieved, over and out

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 alert("I'm working with JavaScript");
+function mad_libs() {
+	document.write ("My stomach was knots with excitement. I couldn't believe that I was going to my first, ever concert. I was just graduating gradeschool and my big (older) brother surpised me with a graduation gift. Tickets to see The Police and The Go-Gos. Totally 80s and I LOVED it. That is how my love of music began.")
+}
+
+mad_libs ();
