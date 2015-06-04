4d8d27f8d865e5dfdadbd810678c2b6b754f9f9c
tcullen <traceycw@comcast.net>
2015-06-04 16:27:10 -0700

    partial step 5

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 alert("I'm working with JavaScript");
+
 function mad_libs() {
-	document.write ("My stomach was knots with excitement. I couldn't believe that I was going to my first, ever concert. I was just graduating gradeschool and my big (older) brother surpised me with a graduation gift. Tickets to see The Police and The Go-Gos. Totally 80s and I LOVED it. That is how my love of music began.")
+	var nouns = ["stomach", "concert", "kid", "gradeschool", "brother", "gift", "The Police", "The Go-Gos", "music"];
+	var adjecives = ["knots", "excitement", "first", "little", "big", "older", "Totally"];
+	var adverbs = ["Totally"]
+
+	document.write("My " + nouns[0] + "was in " + adjecives[0] + "with " + adjecives[1] + ". I couldn't believe that I was going to my " + adjecives[2] +", ever " + nouns[1] + ". I was just a " + adjecives[3] + nouns[2] + "graduating from " + nouns[3] + "and my " + adjecives[4] + adjecives[5] +nouns[4] + "surpised me with a graduation " + nouns[5] +". Tickets to see " + nouns[6] + "and " + nouns[7] + "." + adverbs[0] + "80s and I LOVED it. That is how my love of " + nouns[8] + "began.");
+
 }
+
+
 
 mad_libs ();
